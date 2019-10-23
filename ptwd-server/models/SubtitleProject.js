747391a215cde25523ca702171a6768f25058b32
Alexander Hoinville <ahoin001@gmail.com>
2019-10-23 01:30:15 -0400

    Can upload videos now, still needs to be fixed further

--- ptwd-server/models/SubtitleProject.js
+++ ptwd-server/models/SubtitleProject.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const subtitleProjectSchema = new Schema({
+  videoURL: {
+    type: String
+  },
   userId: {
     type: mongoose.Schema.Types.ObjectId, ref: 'User'
   },
--- ptwd-server/routes/subtitle.routes.js
+++ ptwd-server/routes/subtitle.routes.js
@@ -1,14 +1,69 @@
 const express = require('express');
 const subtitleRouter = express.Router();
-const SubtitleProject = require('../models/SubtitleProject');
+const SubtitleProject = require("../models/SubtitleProject");
 
-subtitleRouter.get('/dashboard/:id', (req,res,next) => {
+// // This package allows access to uploaded files from req.files
+const fileUpload = require('express-fileupload');
+
+// Import CLoudinary from config files where we set data
+const cloudinary = require('../configs/cloudinaryconfig');
+
+subtitleRouter.get('/dashboard/:id', (req, res, next) => {
   const id = req.params.id;
   console.log(id);
   res.render('index');
 });
 
+// TODO Where is this on my local?
+// Use temp files instead of memory for managing the upload process.
+subtitleRouter.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 
+// Test get route to pull user object id
+subtitleRouter.get('/subtitleroute', (req, res, next) => {
+  console.log('this is the sub route ');
+  res.render('index');
+});
+
+// Test post route to create collections in database, adding the current 
+// Add upload.single to middleware chain
+subtitleRouter.post("/createsub", (req, res, next) => {
+
+  // In Postman, fileName is the key we use to get the value ( of file) that was uploaded
+  const theFile = req.files.fileName;
+  let videoURL = '';
+
+  console.log(" form data-------------------------------------: ",
+    req.body, theFile);
+
+  console.log(" Entering cloudinary method data------------------------------------ ");
+
+  cloudinary.uploader.upload(theFile.tempFilePath, { resource_type: "video" },
+    function (error, result) {
+
+      console.log(" Starting  cloudinary method data------------------------------------ ");
+      console.log('error', error);
+      console.log('result', result);
+      console.log(" Exiting cloudinary method data------------------------------------ ");
+
+      videoURL = result.url;
+    });
+
+  SubtitleProject
+    .create({ videoURL: videoURL }) //creates new subtitle document in DB with this info
+    .then(userDoc => {
+
+      // 
+      console.log('Successfully saved video url!');
+      console.log(userDoc);
+
+
+    })
+    .catch(err => next(err)); // close Dummy.create()
+
+});
 
 
 
