da73965774be571f59b32479dce6da55c94f7b23
Alejandro Salas <asalas.wd@outlook.com>
2019-10-22 23:29:50 -0400

    complete create subtitle project route

--- ptwd-server/routes/subtitle.routes.js
+++ ptwd-server/routes/subtitle.routes.js
@@ -3,15 +3,12 @@ const subtitleRouter = express.Router();
 const SubtitleProject = require('../models/SubtitleProject');
 
 subtitleRouter.get('/dashboard', (req,res,next) => {
-  const id = req.user._id;
-  console.log(id);
+  
   res.render('index');
 });
 
 subtitleRouter.post('/dashboard/create-sub', (req,res,next) => {
-  const id = req.user._id;
-  console.log('This is the id the I have in the params: ' + id);
-
+  // Creating subtitle project with req.user._id to find collection 
   const { userId = req.user._id, title, genre, description, createdBy = req.user.fullName, language } = req.body;
   SubtitleProject
   .create({ userId , title, genre, description, createdBy , language }) //creates a new subtitle project in the database
