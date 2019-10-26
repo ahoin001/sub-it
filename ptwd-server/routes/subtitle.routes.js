2e91296674a3d12aeb40f2fbd518b871a5e1362c
Alejandro Salas <asalas.wd@outlook.com>
2019-10-26 14:03:00 -0400

    start sub delete router

--- ptwd-server/routes/subtitle.routes.js
+++ ptwd-server/routes/subtitle.routes.js
@@ -17,18 +17,31 @@ const Subtitle = require("../models/Subtitle");
  * *****************************************************/
 
 subtitleRouter.post('/:projectId/add-sub', (req,res,next) => {  
-  console.log('This is my new sub and the project ID is: ' + req.params.projectId);
+  // TODO (in React): add projectId to the URL
   const {projectId = req.params.projectId, inTime, outTime, text } = req.body;
   Subtitle
         .create({projectId, inTime, outTime, text }) //adds new subtitle to current project
         .then(projectDocument => {
 
-          res.status(401).json({ message: "Subtitle added" });          
-          console.log(`=======================================================`);
+          res.status(401).json({ message: "Subtitle added" });         
+          
           
         }).catch(err => next(err))
         .catch(err => next(err));
+  // Rendering index as placeholder
   res.render('index');  
 });
+
+/*******************************************************
+ * 
+ *                   GET ROUTES
+ * 
+ * *****************************************************/
+
+ subtitleRouter.delete('/:subId/delete-sub', (req,res,next) => {
+  let thisSub = req.params.subId;
+  console.log(thisSub);
+
+ });
 
 module.exports = subtitleRouter;
