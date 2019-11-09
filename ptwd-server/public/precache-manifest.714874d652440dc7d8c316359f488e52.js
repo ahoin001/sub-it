062f971b350ce547cd30c7a78a0ab7c92521cb40
Alejandro Salas <asalas.wd@outlook.com>
2019-11-09 15:16:56 -0500

    complete deployment

---
+++ ptwd-server/public/precache-manifest.714874d652440dc7d8c316359f488e52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48231f5a5d11712ea2b7c15dd66917a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07baddfa996d308c77e3",
+    "url": "/static/css/main.900bb0a1.chunk.css"
+  },
+  {
+    "revision": "c9eba8b203e7e04be991",
+    "url": "/static/js/2.f1f74703.chunk.js"
+  },
+  {
+    "revision": "07baddfa996d308c77e3",
+    "url": "/static/js/main.9014db93.chunk.js"
+  },
+  {
+    "revision": "548c75c6291d24c1e9f6",
+    "url": "/static/js/runtime-main.d8da14d8.js"
+  }
+]);
